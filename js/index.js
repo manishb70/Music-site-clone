4c9068d2c178b96f839a5ab664afc92769db16b2
manishb70 <mb8882568627@gmail.com>
2024-03-29 00:27:30 +0530

    js updates

---
+++ js/index.js
@@ -0,0 +1,49 @@
+
+
+                // var count = 1
+                // var a = document.getElementById("music1").addEventListener("click",()=>{
+                //     count++
+                   
+                //     if(count%2==0){
+                //     document.getElementById('music1').setAttribute("class","fa fa-pause")
+                    
+                //     }else{
+
+                //         document.getElementById('music1').setAttribute("class","fa fa-circle-play")
+                //     }
+                // })
+
+
+                let count = true;
+          var a =   document.getElementById("music1").addEventListener('click',()=>{
+            
+              if(count){
+                  document.getElementById('music1').setAttribute("class","fa fa-pause")
+                  count = false
+                  
+                  
+                    
+                    }else{
+
+                        document.getElementById('music1').setAttribute("class","fa-regular fa-circle-play")
+                        count = true
+                    }
+                    })
+                
+                
+                function trding(){
+
+                    
+                    if(document.getElementById('chkbox').checked == true){
+                        
+                        document.getElementById('trding').style.margin = '450px auto auto auto'
+                        
+                    }else{
+                        
+                        document.getElementById('trding').style.margin = '14px auto auto auto'
+
+
+                        
+                    }
+                }
+
