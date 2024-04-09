c19d58ea09df0d34e921b8b83e45312077a3e11c
manishb70 <mb8882568627@gmail.com>
2024-04-09 23:43:18 +0530

    sute updated

--- js/index.js
+++ js/index.js
@@ -1,17 +1,6 @@
-// responive design of nav bar
-
-// let songs_list = [
-
-
-//     { songs }
-
-// ]
 var song_index = 2
-
-// let audio_play = document.getElementsByClassName('song_card')[song_index].getElementsByTagName('audio')[0]
-
-
-var startsong=document.getElementsByClassName('song_card')[song_index].getElementsByTagName('audio')[0]
+var play_button = document.getElementById('master-p-pause')
+var startsong = document.getElementsByClassName('song_card')[song_index].getElementsByTagName('audio')[0]
 
 function trding() {
 
@@ -22,22 +11,6 @@ function trding() {
         document.getElementById('trding').style.margin = '14px auto auto auto'
     }
 }
-
-// let song_index = 0;
-
-// var count = 1
-// var a = document.getElementById("music1").addEventListener("click",()=>{
-//     count++
-
-//     if(count%2==0){
-//     document.getElementById('music1').setAttribute("class","fa fa-pause")
-
-//     }else{
-
-//         document.getElementById('music1').setAttribute("class","fa fa-circle-play")
-//     }
-// })
-
 
 let count = true;
 var a = document.getElementById("music1").addEventListener('click', () => {
@@ -68,43 +41,20 @@ var a = document.getElementById("music1").addEventListener('click', () => {
 var songs = Array.from(document.getElementsByClassName('song_card'))
 
 songs.forEach((element, i) => {
-
-    // element.getElementsByTagName('i')[0].setAttribute('id',i)    
-    // document.getElementsByClassName('song_card')[i].setAttribute('id',i)
+    
     element.setAttribute('id', i)
 
 
 });
 
 
-// var songs = 
 
 Array.from(document.getElementsByClassName('trending_music_card')).forEach((element2, i) => {
 
-    // element.getElementsByTagName('i')[0].setAttribute('id',i)    
-    // document.getElementsByClassName('song_card')[i].setAttribute('id',i)
-    element2.setAttribute('id', "m"+i)
+    element2.setAttribute('id', "m" + i)
 
 
 });
-
-
-
-
-
-//     songs.forEach(function(item, index) {
-//         item.addEventListener('click', function() {
-
-//           item.getElementsByTagName('i')[0].setAttribute("class","fa fa-pause")
-//         item.getElementsByTagName('audio')[0].play()
-
-//         // song_index =parseInt(item.getElementsByTagName('i')[0].getAttribute('id'))
-//             console.log(item)
-
-//     });
-
-// });
-
 
 
 
@@ -122,30 +72,33 @@ Array.from(document.getElementsByClassName('song_card')).forEach((element, i) =>
 
             element.getElementsByTagName('audio')[0].play()
             element.getElementsByTagName('i')[0].setAttribute("class", "fa fa-pause")
-            document.getElementsByClassName('play-bar')[0].style.opacity=1
+            document.getElementsByClassName('play-bar')[0].style.opacity = 1
 
-            element.getElementsByTagName('audio')[0].addEventListener('timeupdate',()=>{
+            element.getElementsByTagName('audio')[0].addEventListener('timeupdate', () => {
 
-            songcurrentime=element.getElementsByTagName('audio')[0].currentTime
-            songduration=element.getElementsByTagName('audio')[0].duration
+                songcurrentime = element.getElementsByTagName('audio')[0].currentTime
+                songduration = element.getElementsByTagName('audio')[0].duration
 
-            document.getElementsByClassName('play-bar')[0].getElementsByTagName('input')[0].value=songcurrentime/songduration*100
-            // document.getElementsByClassName('play-bar').value=10
-            // console.log(parseInt(songcurrentime/songduration*100))
-            document.getElementsByClassName('play-bar')[0].getElementsByTagName('i')[1].setAttribute("class", "fa fa-pause fa-2x")
+                document.getElementsByClassName('play-bar')[0].getElementsByTagName('input')[0].value = songcurrentime / songduration * 100
+                document.getElementsByClassName('play-bar')[0].getElementsByTagName('i')[1].setAttribute("class", "fa fa-pause fa-2x")
+                
+                
+            })
             
-        
-        })
-
-
+            
         } else {
 
             element.getElementsByTagName('audio')[0].pause()
             element.getElementsByTagName('i')[0].setAttribute("class", "fa fa-play")
-            document.getElementsByClassName('play-bar')[0].style.opacity=0
+            document.getElementsByClassName('play-bar')[0].style.opacity = 0
             document.getElementsByClassName('play-bar')[0].getElementsByTagName('i')[1].setAttribute("class", "fa-regular fa-circle-play fa-2x")
         }
 
+        play_button.addEventListener('click', () => {
+            pauseallsong()
+            // play_button.setAttribute("class", "fa fa-play fa-2x")
+        
+        })
     })
 
 
@@ -156,46 +109,34 @@ Array.from(document.getElementsByClassName('trending_music_card')).forEach((elem
     element.addEventListener('click', (e) => {
 
         console.log(element.getElementsByTagName('audio')[0])
-        // document.getElementById('Teri').addEventListener('timeupdate',()=>{
-
-        //     songcurrentime=element.getElementsByTagName('audio')[0].currentTime
-        //     songduration=element.getElementsByTagName('audio')[0].duration
-
-        //     document.getElementById('bar').value=songcurrentime/songduration*100
-
-        // })
         console.log(e.target.id)
         if (element.getElementsByTagName('audio')[0].paused) {
             pauseallsongtrending()
-            pauseallsong() 
+            pauseallsong()
 
             element.getElementsByTagName('audio')[0].play()
             element.getElementsByTagName('i')[0].setAttribute("class", "fa fa-pause")
-            document.getElementsByClassName('play-bar')[0].style.opacity=1
-            // progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
-            // console.log(element.getElementsByTagName('audio')[0].currentTime)
-                console.log(document.querySelector('.play-bar'))
-                console.log(document.getElementsByClassName('play-bar')[0])
+            document.getElementsByClassName('play-bar')[0].style.opacity = 1
+            console.log(document.querySelector('.play-bar'))
+            console.log(document.getElementsByClassName('play-bar')[0])
 
 
-                element.getElementsByTagName('audio')[0].addEventListener('timeupdate',()=>{
+            element.getElementsByTagName('audio')[0].addEventListener('timeupdate', () => {
 
-                    songcurrentime=element.getElementsByTagName('audio')[0].currentTime
-                    songduration=element.getElementsByTagName('audio')[0].duration
-        
-                    document.getElementsByClassName('play-bar')[0].getElementsByTagName('input')[0].value=songcurrentime/songduration*100
-                    // document.getElementsByClassName('play-bar').value=10
-                    // console.log(parseInt(songcurrentime/songduration*100))
-                    document.getElementsByClassName('play-bar')[0].getElementsByTagName('i')[1].setAttribute("class", "fa fa-pause fa-2x")
+                songcurrentime = element.getElementsByTagName('audio')[0].currentTime
+                songduration = element.getElementsByTagName('audio')[0].duration
+
+                document.getElementsByClassName('play-bar')[0].getElementsByTagName('input')[0].value = songcurrentime / songduration * 100
+                document.getElementsByClassName('play-bar')[0].getElementsByTagName('i')[1].setAttribute("class", "fa fa-pause fa-2x")
                 
-                })
+            })
 
 
         } else {
 
             element.getElementsByTagName('audio')[0].pause()
             element.getElementsByTagName('i')[0].setAttribute("class", "fa fa-play")
-            document.getElementsByClassName('play-bar')[0].style.opacity=0
+            document.getElementsByClassName('play-bar')[0].style.opacity = 0
             document.getElementsByClassName('play-bar')[0].getElementsByTagName('i')[1].setAttribute("class", "fa-regular fa-circle-play fa-2x")
         }
 
@@ -203,43 +144,18 @@ Array.from(document.getElementsByClassName('trending_music_card')).forEach((elem
 
 
     })
-        
+
 })
 
 
 
 
 
-
-// document.getElementsByClassName('playlist_song_card')[0].addEventListener('click', () => {
-
-
-
-//     console.log("Hello world")
-
-//     pauseallsong()
-
-
-
-// })
-
-
-
-
-
 function pauseallsong() {
-
-
-    // for( i =1; i<10; i++){
-
-    //     document.getElementById(i).getElementsByTagName('audio')[0].pause()
-
-
-    // }
-
     songs.forEach(element => {
         element.getElementsByTagName('audio')[0].pause()
         element.getElementsByTagName('i')[0].setAttribute("class", "fa fa-play")
+        play_button.setAttribute("class", "fa fa-play fa-2x")
     });
 
 
@@ -247,15 +163,6 @@ function pauseallsong() {
 
 
 function pauseallsongtrending() {
-
-
-    // for( i =1; i<10; i++){
-
-    //     document.getElementById(i).getElementsByTagName('audio')[0].pause()
-
-
-    // }
-
     Array.from(document.getElementsByClassName("trending_music_card")).forEach(element => {
         element.getElementsByTagName('audio')[0].pause()
         element.getElementsByTagName('i')[0].setAttribute("class", "fa fa-play")
@@ -280,17 +187,58 @@ function slideimage() {
     });
 
 }
-        function nextimage(){
-            
-            
-            if(counter>document.getElementsByClassName('container')[0].getElementsByTagName('img').length-1){
-                counter=0
-                console.log("value is greater then 3")    
-            }
-            slideimage()
-            counter++
-        }    
-  
+function nextimage() {
 
-setInterval( nextimage,1500)       
+
+    if (counter > document.getElementsByClassName('container')[0].getElementsByTagName('img').length - 1) {
+        counter = 0
+        console.log("value is greater then 3")
+    }
+    slideimage()
+    counter++
+}
+setInterval(nextimage, 1500)
+
+
+
+
+    function autoplay(){
+
+
+        
+            songs.forEach(element => {
+                // conso    le.log(element.getElementsByTagName('audio')[0])
+                var verify=0           
+                if(element.getElementsByTagName('audio')[0].pause){
+
+                    playsongstart() 
+                    // console.log("Yes song is pause")
+                                        
+
+                                }
+                            
+                            
+                        
+                        });
+                        
+        
+        
+
+
+
+    }
+
+
+
+        
+            function playsongstart(){
+
+                song_start_index=0
+                document.getElementsByTagName('audio')[song_start_index].play()
+
+
+            }
+
+
+
 
